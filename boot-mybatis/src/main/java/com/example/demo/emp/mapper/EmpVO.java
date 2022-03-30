0e928ec760b51be50bfd81c5cfcddf60e679c0e8
Byung <qudgh4290@gmail.com>
2022-03-30 17:43:31 +0900

    spring boot : mybatis study

---
+++ boot-mybatis/src/main/java/com/example/demo/emp/mapper/EmpVO.java
@@ -0,0 +1,20 @@
+package com.example.demo.emp.mapper;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EmpVO { 
+	String employeeId;
+	String firstName;
+	String lastName;
+	String email;
+	String hireDate;
+	String jobId;
+	String departmentId;
+	String profile;
+	MultipartFile uploadFile;
+} 
